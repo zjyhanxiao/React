b8aa1c8db79ba1140683001417cea52b6f12823d
yangyang <yang.yang@meixinfinance.com>
2016-12-16 10:35:36 +0800

    signature样式-01

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,33 +10,32 @@ import Signature from 'components/signature/index'
 const FormItem = Form.Item;
 const Option = Select.Option;
 
+//const residences = [{
+//  value: 'zhejiang',
+//  label: 'Zhejiang',
+//  children: [{
+//    value: 'hangzhou',
+//    label: 'Hangzhou',
+//    children: [{
+//      value: 'xihu',
+//      label: 'West Lake',
+//    }],
+//  }],
+//}, {
+//  value: 'jiangsu',
+//  label: 'Jiangsu',
+//  children: [{
+//    value: 'nanjing',
+//    label: 'Nanjing',
+//    children: [{
+//      value: 'zhonghuamen',
+//      label: 'Zhong Hua Men',
+//    }],
+//  }],
+//}];
 function handleChange(value) {
   console.log(`selected ${value}`);
 }
-
-const residences = [{
-  value: 'zhejiang',
-  label: 'Zhejiang',
-  children: [{
-    value: 'hangzhou',
-    label: 'Hangzhou',
-    children: [{
-      value: 'xihu',
-      label: 'West Lake',
-    }],
-  }],
-}, {
-  value: 'jiangsu',
-  label: 'Jiangsu',
-  children: [{
-    value: 'nanjing',
-    label: 'Nanjing',
-    children: [{
-      value: 'zhonghuamen',
-      label: 'Zhong Hua Men',
-    }],
-  }],
-}];
 
 const RegistrationForm = Form.create()(React.createClass({
   getInitialState() {
@@ -94,44 +93,51 @@ const RegistrationForm = Form.create()(React.createClass({
 
       <div style={{ width: 900 ,background:'#fff',overflow:'hidden'}}>
 
-
         <Form horizontal onSubmit={this.handleSubmit}>
+          <FormItem>
+            <Row style={{paddingTop:'30px'}}>
+              <Col span={2} offset={2}><h2 style={{color: '#159bd6',fontFamily:'宋体'}}>个人信息</h2></Col>
+              <Col span={11} offset={1}><p style={{color: '#ff6600',fontFamily:'宋体',marginTop:'5px'}}>投资人姓名必须与汇款银行帐号上的姓名一致,且一旦提交不可修改。</p></Col>
+            </Row>
+          </FormItem>
 
-          <Row style={{paddingTop:'30px',paddingBottom:'30px'}}>
-            <Col span={2} offset={2}><h2 style={{color: '#159bd6',fontFamily:'宋体'}}>个人信息</h2></Col>
-            <Col span={11} offset={1}><p style={{color: '#ff6600',fontFamily:'宋体',marginTop:'5px'}}>投资人姓名必须与汇款银行帐号上的姓名一致,且一旦提交不可修改。</p></Col>
+
+
+          <Row style={{marginTop:'10px'}}>
+
+
           </Row>
+
+
 
           <FormItem
             {...formItemLayout}
             label="姓氏"
             hasFeedback
           >
-            {getFieldDecorator('email', {
+            {getFieldDecorator('last_name', {
               rules: [{
-                type: 'email', message: 'The input is not valid E-mail!',
+                required: true, message: '请输入你的姓的汉语拼音!',
               }, {
-                required: true, message: 'Please input your E-mail!',
+                validator: this.checkConfirm,
               }],
             })(
-              <Input />
+              <Input type="text" onBlur={this.handlePasswordBlur} />
             )}
           </FormItem>
-
-
           <FormItem
             {...formItemLayout}
             label="名"
             hasFeedback
           >
-            {getFieldDecorator('password', {
+            {getFieldDecorator('first_name', {
               rules: [{
-                required: true, message: 'Please input your password!',
+                required: true, message: '请输入你的名的汉语拼音!',
               }, {
                 validator: this.checkConfirm,
               }],
             })(
-              <Input type="password" onBlur={this.handlePasswordBlur} />
+              <Input type="text" onBlur={this.handlePasswordBlur} />
             )}
           </FormItem>
           <FormItem
@@ -139,11 +145,11 @@ const RegistrationForm = Form.create()(React.createClass({
             label="出生日期"
             hasFeedback
           >
-            {getFieldDecorator('confirm', {
+            {getFieldDecorator('date_of_birth', {
               rules: [{
-                required: true, message: 'Please confirm your password!',
+                //required: true, message: '请输入你的出生日期！',
               }, {
-                validator: this.checkPassowrd,
+                //validator: this.checkPassowrd,
               }],
             })(
               <DatePickerComponent></DatePickerComponent>
@@ -152,143 +158,170 @@ const RegistrationForm = Form.create()(React.createClass({
           <FormItem
             {...formItemLayout}
             label="出生国家"
-            hasFeedback
           >
-            {getFieldDecorator('confirm', {
-              initialValue:'jack',
-              rules: [{
-                required: true, message: 'Please confirm your password!',
-              }, {
-                validator: this.checkPassowrd,
-              }],
+            {getFieldDecorator('country_of_birth', {
+              initialValue: ['lucy', 'jack', 'disabled','yiminghe'],
+              rules: [{ type: 'string', required: true, message: 'Please select your habitual residence!' }],
             })(
+              //<Cascader options={residences} />
+              <Select size="large" style={{ width: 200 }} onChange={handleChange}>
+                <Option value="jack">Jack</Option>
+                <Option value="lucy">Lucy</Option>
+                <Option value="disabled">Disabled</Option>
+                <Option value="yiminghe">Yiminghe</Option>
+              </Select>
 
             )}
           </FormItem>
           <FormItem
             {...formItemLayout}
             label="纳税国"
-            labelCol='{span: 3, offset: 12}'
-            hasFeedback
           >
-            {getFieldDecorator('confirm', {
-              rules: [{
-                required: true, message: 'Please confirm your password!',
-              }, {
-                validator: this.checkPassowrd,
-              }],
+            {getFieldDecorator('tax_country', {
+              initialValue: ['lucy', 'jack', 'disabled','yiminghe'],
+              rules: [{ type: 'string', required: true, message: 'Please select your habitual residence!' }],
             })(
-              <Input type="text" />
+              //<Cascader options={residences} />
+              <Select size="large" style={{ width: 200 }} onChange={handleChange}>
+                <Option value="jack">Jack</Option>
+                <Option value="lucy">Lucy</Option>
+                <Option value="disabled">Disabled</Option>
+                <Option value="yiminghe">Yiminghe</Option>
+              </Select>
+
             )}
           </FormItem>
           <FormItem
             {...formItemLayout}
             label="国籍"
-            hasFeedback
           >
-            {getFieldDecorator('confirm', {
-              rules: [{
-                required: true, message: 'Please confirm your password!',
-              }, {
-                validator: this.checkPassowrd,
-              }],
+            {getFieldDecorator('nationality', {
+              initialValue: ['lucy', 'jack', 'disabled','yiminghe'],
+              rules: [{ type: 'string', required: true, message: 'Please select your habitual residence!' }],
             })(
-              <Input type="password" />
+              //<Cascader options={residences} />
+              <Select size="large" style={{ width: 200 }} onChange={handleChange}>
+                <Option value="jack">Jack</Option>
+                <Option value="lucy">Lucy</Option>
+                <Option value="disabled">Disabled</Option>
+                <Option value="yiminghe">Yiminghe</Option>
+              </Select>
+
             )}
           </FormItem>
           <FormItem
             {...formItemLayout}
             label="行业"
-            hasFeedback
           >
-            {getFieldDecorator('confirm', {
-              rules: [{
-                required: true, message: 'Please confirm your password!',
-              }, {
-                validator: this.checkPassowrd,
-              }],
+            {getFieldDecorator('residence', {
+              initialValue: ['lucy', 'jack', 'disabled','yiminghe'],
+              rules: [{ type: 'string', required: true, message: 'Please select your habitual residence!' }],
             })(
-              <DatePickerComponent></DatePickerComponent>
+              //<Cascader options={residences} />
+              <Select size="large" style={{ width: 200 }} onChange={handleChange}>
+                <Option value="jack">Jack</Option>
+                <Option value="lucy">Lucy</Option>
+                <Option value="disabled">Disabled</Option>
+                <Option value="yiminghe">Yiminghe</Option>
+              </Select>
+
             )}
           </FormItem>
           <FormItem
             {...formItemLayout}
             label="职业"
-            hasFeedback
           >
-            {getFieldDecorator('confirm', {
-              rules: [{
-                required: true, message: 'Please confirm your password!',
-              }, {
-                validator: this.checkPassowrd,
-              }],
+            {getFieldDecorator('occupation', {
+              initialValue: ['lucy', 'jack', 'disabled','yiminghe'],
+              rules: [{ type: 'string', required: true, message: 'Please select your habitual residence!' }],
             })(
-              <DatePickerComponent></DatePickerComponent>
+              //<Cascader options={residences} />
+              <Select size="large" style={{ width: 200 }} onChange={handleChange}>
+                <Option value="jack">Jack</Option>
+                <Option value="lucy">Lucy</Option>
+                <Option value="disabled">Disabled</Option>
+                <Option value="yiminghe">Yiminghe</Option>
+              </Select>
+
             )}
           </FormItem>
           <FormItem
             {...formItemLayout}
             label="资金来源"
-            hasFeedback
           >
-            {getFieldDecorator('confirm', {
-              rules: [{
-                required: true, message: 'Please confirm your password!',
-              }, {
-                validator: this.checkPassowrd,
-              }],
+            {getFieldDecorator('source_of_income', {
+              initialValue: ['lucy', 'jack', 'disabled','yiminghe'],
+              rules: [{ type: 'string', required: true, message: 'Please select your habitual residence!' }],
             })(
-              <DatePickerComponent></DatePickerComponent>
+              //<Cascader options={residences} />
+              <Select size="large" style={{ width: 200 }}>
+                <Option value="jack">Jack</Option>
+                <Option value="lucy">Lucy</Option>
+                <Option value="disabled">Disabled</Option>
+                <Option value="yiminghe">Yiminghe</Option>
+              </Select>
+
             )}
+
           </FormItem>
-          <Row style={{marginTop:'30px',paddingBottom:'30px'}}>
-            <Col span={2} offset={2} style={{width:'900px'}}><h2 style={{color: '#159bd6',fontFamily:'宋体',height:'30px'}}>证件上传</h2></Col>
-          </Row>
+          <FormItem>
+            <Row style={{marginTop:'30px'}}>
+              <Col span={2} offset={2}><h2 style={{color: '#159bd6',fontFamily:'宋体',height:'30px'}}>证件上传</h2></Col>
+            </Row>
+          </FormItem>
           <FormItem
             {...formItemLayout}
             label="护照号"
             hasFeedback
           >
-            {getFieldDecorator('confirm', {
+            {getFieldDecorator('passport_code', {
               rules: [{
-                required: true, message: 'Please confirm your password!',
+                required: true, message: '请输入你的护照号!',
               }, {
-                validator: this.checkPassowrd,
+                validator: this.checkConfirm,
               }],
             })(
-              <DatePickerComponent></DatePickerComponent>
+              <Input type="text" onBlur={this.handlePasswordBlur} />
             )}
           </FormItem>
           <FormItem
             {...formItemLayout}
-            label="有效期"
+            label="有效期至"
             hasFeedback
           >
             {getFieldDecorator('confirm', {
               rules: [{
-                required: true, message: 'Please confirm your password!',
+                //required: true, message: '请输入你的出生日期！',
               }, {
-                validator: this.checkPassowrd,
+                //validator: this.checkPassowrd,
               }],
             })(
               <DatePickerComponent></DatePickerComponent>
             )}
           </FormItem>
-          <Row style={{marginTop:'80px'}}>
-            <Col span={10} offset={7}><Avatar></Avatar></Col>
-          </Row>
-          <Row style={{marginTop:'20px'}}>
-            <Col span={12} offset={6} style={{color:'#999999',fontFamily:'宋体',textAlign:'center'}}>如果您的护照证件 (例如:旧版中国护照) 不包含签名部分,请将证件置于白纸之上,</Col>
-          </Row>
-          <Row>
-            <Col span={12} offset={6} style={{color:'#999999',fontFamily:'宋体',textAlign:'center'}}>并于证件下方签名, 拍照或扫描上传。证件必须为原件, 不能为复印件。</Col>
-          </Row>
-
+          <FormItem>
+            <Row style={{paddingTop:'30px'}}>
+              <Row style={{marginTop:'40px'}}>
+                <Col span={10} offset={7}><Avatar></Avatar></Col>
+              </Row>
+            </Row>
+          </FormItem>
+          <FormItem>
+            <Row style={{marginTop:'20px'}}>
+              <Col span={12} offset={6} style={{color:'#999999',fontFamily:'宋体',textAlign:'center'}}>如果您的护照证件 (例如:旧版中国护照) 不包含签名部分,请将证件置于白纸之上,</Col>
+            </Row>
+            <Row>
+              <Col span={12} offset={6} style={{color:'#999999',fontFamily:'宋体',textAlign:'center'}}>并于证件下方签名, 拍照或扫描上传。证件必须为原件, 不能为复印件。</Col>
+            </Row>
+          </FormItem>
           <FormItem {...tailFormItemLayout}>
-            <Button type="primary" htmlType="submit" size="large">Register</Button>
+            <Row style={{marginTop:'50px',paddingBottom:'40px'}}>
+              <Col span={8} offset={7}>
+                <Button style={{width: '120px',height:'50px',borderRadius:'30px',background:'#223976',color:'#fff',fontSize:'18px'}} type="primary" htmlType="submit" size="large">下一步</Button>
+               </Col>
+            </Row>
           </FormItem>
         </Form>
-
       </div>
     );
   },
