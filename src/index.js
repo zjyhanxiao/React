f2d731c5adc2ba2fef7925093f1d5f8a663fb1f9
jingyu.zhang@meixinfinance.com <jingyu.zhang@meixinfinance.com>
2016-12-20 16:30:05 +0800

    redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/Main'
 
-import BasicInformation from './components/BasicInformation';
-import AddressInformation from './components/AddressInformation';
-import BankInformation from './components/BankInformation';
+
 
 // Render the main component into the dom
 ReactDOM.render(
   <div>
-    <BasicInformation></BasicInformation>
-    <AddressInformation></AddressInformation>
-    <BankInformation></BankInformation>
+    <App/>
   </div>
 
   , document.getElementById('app'));
