b90225abe07e80574e3a5f9c04c641ade830bd23
jingyu.zhang@meixinfinance.com <jingyu.zhang@meixinfinance.com>
2016-12-20 16:29:52 +0800

    redux

---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import BasicInfo from './BasicInfo/index';
+
+class Main extends React.Component {
+    render() {
+        return (
+            <BasicInfo />
+        );
+    }
+}
+
+Main.defaultProps = {};
+
+export default Main;
