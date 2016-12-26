29d291c66b9324c07abc6e1df536d40a4db07a8b
jingyu.zhang@meixinfinance.com <jingyu.zhang@meixinfinance.com>
2016-12-26 12:19:16 +0800

    Merge branch 'dev' of git.meixincn.com:web/acadia into dev
    # Conflicts:
    #	src/components/basic/BasicInfo.js
    #	src/index.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux'
 import configureStore from '../Redux/stores/index'
-import Basic from './basic/index'
+import AsyncApp from './BasicInformation.js'
 
 const store = configureStore()
 export default class Main extends Component {
     render() {
         return (
             <Provider store={store}>
-              <Basic></Basic>
+              <AsyncApp></AsyncApp>
             </Provider>
         )
     }
--- src/components/basic/index.js
+++ src/components/basic/index.js
@@ -1,34 +1,72 @@
 /**
- * Created by robot on 2016/12/23.
+ * Created by robot on 2016/12/20.
  */
-import 'antd/dist/antd.css';
-require('./BasicInfo.css');
+// require('./BasicInfo.css');
 
+import 'antd/dist/antd.min.css'
 import React from 'react';
-import Basicidcard from './Basicidcard';
-import BasicInfo from './BasicInfo';
-import BasicInfoUC from './BasicInfoUC';
-import Basicpassport from './Basicpassport';
+import {Form, Input,Select, Row, Col, Button,DatePicker} from 'antd';
 
-class Basic extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      profile:{}
-    }
-  }
 
-  render() {
-    return (
-      <div>
+const FormItem = Form.Item;
+const Option = Select.Option;
 
-        <BasicInfoUC></BasicInfoUC>
-        <Basicpassport></Basicpassport>
-      </div>
-    );
-  }
+function handleChange(value) {
+    console.log(`selected ${value}`);
 }
+const BasicInfo = Form.create({
+    onFieldsChange(props, changedFields) {
+        props.onChange(changedFields);
+    },
+    mapPropsToFields(props) {
+        return {
+            first_name: {
+                ...props.getsByProfile.first_name,
+                value: props.getsByProfile.first_name,
+            },
+            email: {
+                ...props.getsByProfile.email,
+                value: props.getsByProfile.email,
+            },
+        };
+    },
 
-Basic.defaultProps = {};
+})((props) => {
+    const { getFieldDecorator } = props.form;
+    return (
+        <Form inline>
+          <FormItem label="first_name">
+              {getFieldDecorator('first_name', {
+                  rules: [{ required: true, message: 'first_name is required!' }],
+              })(<Input />)}
+          </FormItem>
+          <FormItem label="email">
+              {getFieldDecorator('email', {
+                  rules: [{ required: true, message: 'email is required!' }],
+              })(<Input />)}
+          </FormItem>
+        </Form>
+    );
+});
 
-export default Basic;
+BasicInfo.defaultProps = {};
+
+export default BasicInfo;
+
+
+/********************
+ <FormItem {...tailFormItemLayout}>
+ <Row style={{marginTop: '50px', paddingBottom: '40px'}}>
+ <Col span={8} offset={7}>
+ <Button style={{
+        width: '120px',
+        height: '50px',
+        borderRadius: '30px',
+        background: '#223976',
+        color: '#fff',
+        fontSize: '18px'
+      }} type="primary" htmlType="submit" onClick= {this.handleSubmit.bind(this)} size="large">下一步</Button>
+ </Col>
+ </Row>
+ </FormItem>
+ *********************/
