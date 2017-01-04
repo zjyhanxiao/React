ccb44d24765665a2f4d7c0f31f9781923a45df0a
jingyu.zhang@meixinfinance.com <jingyu.zhang@meixinfinance.com>
2017-01-04 16:03:43 +0800

    Merge branch 'dev' of git.meixincn.com:web/acadia into dev
    # Conflicts:
    #	src/components/basic/BasicInfo.js
    #	src/index.js

--- src/Redux/reducer/index.js
+++ src/Redux/reducer/index.js
@@ -8,9 +8,6 @@ const baseState={
 const getsProfile = (state = {baseState}, action) => {
     switch (action.type) {
         case types.GET_PROFILE_SUCCESS:
-
-            // console.log('BBBBBBBBBBBBBB' + JSON.stringify(state))
-            // console.log('bbbbbbbbbbbbbbbb' + JSON.stringify(action))
             return Object.assign({}, state, {
                 /*first_name: action.json.body.first_name,
                  email: action.json.body.email,
@@ -19,8 +16,6 @@ const getsProfile = (state = {baseState}, action) => {
                 base_profile: action.json.body
             });
         case types.UPDATE_UPLOADER:
-            // console.log('CCCCCCCCCCCCCCCCCCCC' + JSON.stringify(state))
-            // console.log('ccccccccccccccccccc' + JSON.stringify(action))
             return Object.assign({}, state, {
                 base_profile: {
                     ...state.base_profile,
@@ -28,8 +23,6 @@ const getsProfile = (state = {baseState}, action) => {
                 }
             });
         case types.SAVE_FIELDS:
-            // console.log('CCCCCCCCCCCCCCCCCCCC' + JSON.stringify(state))
-            // console.log('ccccccccccccccccccc' + JSON.stringify(action.val))
             return Object.assign({}, state, {base_profile: {
                 ...state.base_profile,
                 [action.key]: action.val
