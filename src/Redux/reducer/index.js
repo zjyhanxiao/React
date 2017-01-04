3e5f848dd0615dca8ac0b833dc57296ab0b15b4c
jingyu.zhang@meixinfinance.com <jingyu.zhang@meixinfinance.com>
2017-01-04 16:17:10 +0800

    Merge branch 'redux' of git.meixincn.com:web/acadia into dev
    # Conflicts:
    #	src/Redux/reducer/index.js
    #	src/components/Capacity/indexButton.js
    #	src/view/BasicInformation.js

--- src/Redux/reducer/index.js
+++ src/Redux/reducer/index.js
@@ -1,9 +1,8 @@
 import {combineReducers} from 'redux'
 import * as types from '../actions/ActionTypes'
-
-const getsProfile = (state = {}, action) => {
-  console.log('BBBBBBBBBBBBBBBBBBBBBBBB'+JSON.stringify(state))
-
+const baseState={
+}
+const getsProfile = (state = {baseState}, action) => {
     switch (action.type) {
         case types.GET_PROFILE_SUCCESS:
             return Object.assign({}, state, {
@@ -11,23 +10,26 @@ const getsProfile = (state = {}, action) => {
                  email: action.json.body.email,
                  date_of_birth: action.json.body.date_of_birth,
                  passport_photo: null*/
-                base_profile: {
-                  [action.data]: action.key
-                }
+                base_profile: action.json.body
             });
         case types.UPDATE_UPLOADER:
             return Object.assign({}, state, {
                 base_profile: {
+                    ...state.base_profile,
                     [action.data]: action.key
                 }
+            });
+        case types.SAVE_FIELDS:
+            return Object.assign({}, state, {base_profile: {
+                ...state.base_profile,
+                [action.key]: action.val
+            }
             });
         default:
             return state
     }
 };
-
 const rootReducer = combineReducers({
     getsProfile
 });
-
 export default rootReducer
--- src/view/BasicInformation.js
+++ src/view/BasicInformation.js
@@ -2,42 +2,44 @@ import React, {Component, PropTypes}from 'react';
 import 'antd/dist/antd.css'
 import {Form, Row, Col, Button} from 'antd';
 import {connect} from 'react-redux'
+import {bindActionCreators} from 'redux'
 import moment from 'moment'
-import {updateProfile} from '../Redux/actions/index'
+import {updateProfile, saveFields} from '../Redux/actions/index'
 import BasicInfoUC from '../components/basic/BasicInfoUC'
 import BasicInfoH from '../components/basic/BasicInfoH'
 import Basicpassport from '../components/basic/Basicpassport'
 const FormItem = Form.Item;
-
 class BasicInformation extends React.Component {
     constructor(props) {
         super(props)
     }
-
-    goback(){
-      console.log('1')
-    }
-
     handleSubmit(e) {
-        console.log('GGGGGGGGGGGGGGG'+JSON.stringify(this.props))
         e.preventDefault();
         this.props.form.validateFieldsAndScroll((err, values) => {
             if (!err) {
                 values.date_of_birth = values.date_of_birth.format('YYYY-MM-DD')
-                values.mx_token='25b6ca3901730fba2cb6098d34912f34'
-                values.mx_secret='da9d83c022637e7eda9fb59299026e7c'
+                values.mx_token = '25b6ca3901730fba2cb6098d34912f34'
+                values.mx_secret = 'da9d83c022637e7eda9fb59299026e7c'
                 console.log('Received values of form: ', JSON.stringify(values));
-
-                const { dispatch } = this.props
-                dispatch(updateProfile(values,this.success))
+                /*const {dispatch} = this.props
+                dispatch(updateProfile(values, this.success))*/
             }
         });
     }
-
-    success(){
+    disabledDate(current) {
+        // can not select days before today and today
+        return current && current.valueOf() < Date.now() - 90 * 24 * 60 * 60 * 1000;
+    }
+    expire_date(current) {
+        // can not select days before today and today
+        return current && current.valueOf() < Date.now() - 1 * 24 * 60 * 60 * 1000;
+    }
+    componentWillReceiveProps(nextProps) {
+        // console.log('1111111111111'+JSON.stringify(nextProps))
+    }
+    success() {
         console.log('success')
     }
-
     render() {
         // const {getFieldDecorator} = this.props.form;
         const formItemLayout = {
@@ -54,67 +56,51 @@ class BasicInformation extends React.Component {
             <div style={{width: 900, background: '#fff', overflow: 'hidden'}}>
                 {JSON.stringify(this.props)}
                 <Form horizontal>
-                    <BasicInfoH {...this.props} getFieldDecorator={this.props.form}  />
+                    <BasicInfoH {...this.props} getFieldDecorator={this.props.form}
+                                disabledDate={this.disabledDate} expire_date={this.expire_date}/>
                     {/*<BasicInfoUC {...this.props} getFieldDecorator={this.props.form} />*/}
-                    <Basicpassport {...this.props}  getFieldDecorator={this.props.form} />
-
-
-                  <Row style={{marginTop: '50px', paddingBottom: '40px'}}>
-                    <Col span={3} offset={6}>
-                      <Button style={{
-                      width: '120px',
-                      height: '50px',
-                      borderRadius: '30px',
-                      background: '#ffffff',
-                      color: '#223976',
-                      fontSize: '18px'
-                    }} type="primary" onClick={this.goback} size="large">上一步</Button>
-                    </Col>
-
-                    <Col span={3} offset={6}>
-                      <FormItem {...tailFormItemLayout}>
-                        <Button style={{
-                            width: '120px',
-                            height: '50px',
-                            borderRadius: '30px',
-                            background: '#223976',
-                            color: '#fff',
-                            fontSize: '18px'
-                        }} type="primary" htmlType="submit" onClick={this.handleSubmit.bind(this)} size="large">下一步</Button>
-                      </FormItem>
-                    </Col>
-                  </Row>
-
+                    <Basicpassport {...this.props} getFieldDecorator={this.props.form} />
+                    <FormItem {...tailFormItemLayout}>
+                        <Row style={{marginTop: '50px', paddingBottom: '40px'}}>
+                            <Col span={4} offset={7}>
+                                <Button style={{
+                                    width: '120px',
+                                    height: '50px',
+                                    borderRadius: '30px',
+                                    background: '#223976',
+                                    color: '#fff',
+                                    fontSize: '18px'
+                                }} type="primary" htmlType="submit" onClick={this.handleSubmit.bind(this)} size="large">下一步</Button>
+                            </Col>
+                        </Row>
+                    </FormItem>
                 </Form>
-
             </div>
         );
     }
 }
-
 BasicInformation = Form.create({
-    mapPropsToFields(props) {
-        return {
-            passport_photo: {
-                ...props.passport_photo,
-                value: props.passport_photo,
-            },
-        };
+    onFieldsChange(props, changedFields) {
+        for (let i in changedFields) {
+            let key = changedFields[i].name
+            let val = changedFields[i].value
+            if (key == 'date_of_birth') {
+                let val = changedFields[i].value.format('YYYY-MM-DD')
+                props.dispatch(saveFields(key, val));
+            } else {
+                props.dispatch(saveFields(key, val));
+            }
+        }
     }
 })(BasicInformation);
-
 BasicInformation.defaultProps = {};
-
-BasicInformation.propTypes = {
-    dispatch: PropTypes.func.isRequired
-}
-
+/*BasicInformation.propTypes = {
+ dispatch: PropTypes.func.isRequired
+ }*/
 const mapStateToProps = (state) => {
     return {
-        // getsProfile: state.getsProfile.base_profile
+        // getsProfile: state.getsProfile
         // base_profile:state.getsProfile.base_profile
     }
 }
-
-
 export default connect(mapStateToProps)(BasicInformation)
