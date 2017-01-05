82cee18c1467417191ae68cee8cba1cace5cc949
yangyang <yang.yang@meixinfinance.com>
2017-01-05 19:10:44 +0800

    Merge branch 'dev' of git.meixincn.com:web/acadia into dev
    # Conflicts:
    #	src/components/infoPages/itemAmount.js

--- src/components/bank/BankFast.js
+++ src/components/bank/BankFast.js
@@ -46,7 +46,8 @@ class BankFast extends React.Component {
             </div>
             <div style={{width: 180,height:55,position: 'absolute',top:'15px',right:'0px'}}>
               <p style={{lineHeight:'1.5'}}>中国银行-香港银行</p>
-              <p style={{lineHeight:'1.5'}}>China Merchants Bank (HongKong Branch)</p>
+              <p style={{lineHeight:'1.5'}}>China Merchants Bank (Hong
+                Kong Branch)</p>
             </div>
 
           </Radio.Button>
@@ -54,11 +55,25 @@ class BankFast extends React.Component {
                         style={{width: 260,height:80, float:'left',marginLeft:'20px',marginTop:'20px',borderRadius:'5px',position:'relative'}}>
 
             <div style={{width: 50,height:50,position: 'absolute',top:'15px',left:'15px',lineHeight:'50px'}}>
-              <img style={{width:'100%',}} src="../images/bank-hua.png" alt=""/>
+              <img style={{width:'100%',}} src="../images/bank-xin.jpg" alt=""/>
             </div>
             <div style={{width: 180,height:55,position: 'absolute',top:'15px',right:'0px'}}>
               <p style={{lineHeight:'1.5'}}>中国银行-香港银行</p>
-              <p style={{lineHeight:'1.5'}}>China Merchants Bank (HongKong Branch)</p>
+              <p style={{lineHeight:'1.5'}}>China Merchants Bank (Hong
+                Kong Branch)</p>
+            </div>
+
+          </Radio.Button>
+          <Radio.Button value="投资"
+                        style={{width: 260,height:80, float:'left',marginLeft:'20px',marginTop:'20px',borderRadius:'5px',position:'relative'}}>
+
+            <div style={{width: 50,height:50,position: 'absolute',top:'15px',left:'15px',lineHeight:'50px'}}>
+              <img style={{width:'100%',}} src="../images/bank-xin.jpg" alt=""/>
+            </div>
+            <div style={{width: 180,height:55,position: 'absolute',top:'15px',right:'0px'}}>
+              <p style={{lineHeight:'1.5'}}>中国银行-香港银行</p>
+              <p style={{lineHeight:'1.5'}}>China Merchants Bank (Hong
+                Kong Branch)</p>
             </div>
 
           </Radio.Button>
@@ -67,52 +82,6 @@ class BankFast extends React.Component {
 
 
 
-
-
-
-
-        <ul style={{width: 860, background: '#fff', overflow: 'hidden',margin:'0 auto'}}>
-          <li style={{width: 260,height:80, float:'left',marginLeft:'20px',marginTop:'20px',borderRadius:'5px',position:'relative',border:'1px solid #223976'}}>
-            <div style={{width: 50,height:50,position: 'absolute',top:'15px',left:'15px',lineHeight:'50px'}}>
-              <img style={{width:'100%',}} src="../images/bank-hua.png" alt=""/>
-            </div>
-            <div style={{width: 180,height:55,position: 'absolute',top:'15px',right:'0px'}}>
-              <p style={{color:'#223976'}}>中国银行-香港银行</p>
-              <p style={{color:'#223976'}}>China Merchants Bank (Hong
-                Kong Branch)</p>
-            </div>
-          </li>
-          <li style={{width: 260,height:80, float:'left',marginLeft:'20px',marginTop:'20px',borderRadius:'5px',position:'relative',border:'1px solid #cccccc'}}>
-            <div style={{width: 50,height:50,position: 'absolute',top:'15px',left:'15px',lineHeight:'50px'}}>
-              <img style={{width:'100%',}} src="../images/bank-hua.png" alt=""/>
-            </div>
-            <div style={{width: 180,height:55,position: 'absolute',top:'15px',right:'0px'}}>
-              <p style={{color:'#223976'}}>中国银行-香港银行</p>
-              <p style={{color:'#223976'}}>China Merchants Bank (Hong
-                Kong Branch)</p>
-            </div>
-          </li>
-          <li style={{width: 260,height:80, float:'left',marginLeft:'20px',marginTop:'20px',borderRadius:'5px',position:'relative',border:'1px solid #cccccc'}}>
-            <div style={{width: 50,height:50,position: 'absolute',top:'15px',left:'15px',lineHeight:'50px'}}>
-              <img style={{width:'100%',}} src="../images/bank-xin.jpg" alt=""/>
-            </div>
-            <div style={{width: 180,height:55,position: 'absolute',top:'15px',right:'0px'}}>
-              <p style={{color:'#223976'}}>中国银行-香港银行</p>
-              <p style={{color:'#223976'}}>China Merchants Bank (Hong
-                Kong Branch)</p>
-            </div>
-          </li>
-          <li style={{width: 260,height:80, float:'left',marginLeft:'20px',marginTop:'20px',borderRadius:'5px',position:'relative',border:'1px solid #cccccc'}}>
-            <div style={{width: 50,height:50,position: 'absolute',top:'15px',left:'15px',lineHeight:'50px'}}>
-              <img style={{width:'100%',}} src="../images/bank-hua.png" alt=""/>
-            </div>
-            <div style={{width: 180,height:55,position: 'absolute',top:'15px',right:'0px'}}>
-              <p style={{color:'#223976'}}>中国银行-香港银行</p>
-              <p style={{color:'#223976'}}>China Merchants Bank (Hong
-                Kong Branch)</p>
-            </div>
-          </li>
-        </ul>
         <Row style={{marginTop:'50px'}}>
           <Col span={4} offset={6}><Button style={{width:'100%',height:'45px',fontSize:'18px',borderRadius:'45px'}} type="ghost">取消</Button></Col>
           <Col span={4} offset={4}><Button style={{width:'100%',height:'45px',fontSize:'18px',borderRadius:'45px',background:'#223976'}} type="primary">确定</Button></Col>
