f9ba3e893a79d1c3cc81c53219638a5d8c76660a
yangyang <yang.yang@meixinfinance.com>
2017-01-10 13:59:25 +0800

    Merge branch 'dev' of git.meixincn.com:web/acadia into dev
    # Conflicts:
    #	src/components/infoPages/itemAmount.js

--- src/components/CapacityShow.js
+++ src/components/CapacityShow.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import InformationConfirmation from '../view/InformationConfirmation'
 
+import AddressPlate from './infoPages/addressPlate'
+import BankPlate from './infoPages/bankPlate'
+import BasicPlate from './infoPages/basicPlate'
+import CompliancePlate from './infoPages/compliancePlate'
+
 class CapacityShow extends React.Component {
     constructor() {
         super()
+        this.state = {
+          a: true,
+        }
     }
     render() {
         return (
+
             <div>
-              <InformationConfirmation/>
+              {this.state.a?
+                <InformationConfirmation/>
+                :
+                <div>
+                  <BasicPlate></BasicPlate>
+                  <AddressPlate></AddressPlate>
+                  <BankPlate></BankPlate>
+                  <CompliancePlate></CompliancePlate>
+                </div>
+              }
+
             </div>
         );
     }
