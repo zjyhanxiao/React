07140e181dda8daa600896b69d76a2f5caf412ec
yangyang <yang.yang@meixinfinance.com>
2017-01-11 12:09:27 +0800

    Merge branch 'dev' of git.meixincn.com:web/acadia into dev
    # Conflicts:
    #	src/components/infoPages/itemAmount.js

---
+++ src/components/infoPages/indexPlate.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import AddressPlate from './addressPlate'
+import BankPlate from './bankPlate'
+import BasicPlate from './basicPlate'
+import CompliancePlate from './compliancePlate'
+
+class IndexPlate extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div style={{width: '100%', background: '#fff', overflow: 'hidden'}}>
+        <BasicPlate {...this.props} />
+        <AddressPlate/>
+        <BankPlate/>
+
+        {this.props.getsProfile.base_profile.with_spouse?
+          <CompliancePlate/>
+          :
+          ''
+        }
+
+
+        <CompliancePlate/>
+      </div>
+    );
+  }
+}
+
+IndexPlate.defaultProps = {};
+
+export default IndexPlate;
