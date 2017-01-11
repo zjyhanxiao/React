e35796537a78bcb9fffa390b150a107f310e1b6a
yangyang <yang.yang@meixinfinance.com>
2017-01-11 20:35:22 +0800

    Merge branch 'dev' of git.meixincn.com:web/acadia into dev
    # Conflicts:
    #	src/components/infoPages/indexPlate.js

--- src/components/compliance/CompliancePublic.js
+++ src/components/compliance/CompliancePublic.js
@@ -20,7 +20,7 @@ class CompliancePublic extends React.Component {
     this.state = {
       a:false,
       profile:{},
-      size: '收入审查',
+      size: 'INCOME',
       b:''
     }
   }
@@ -40,6 +40,7 @@ class CompliancePublic extends React.Component {
     this.setState({ size: e.target.value });
     console.log(e.target.value)
   }
+
 
 
 
@@ -82,56 +83,69 @@ class CompliancePublic extends React.Component {
           </FormItem>
 
 
-          <FormItem {...formItemLayout}>
-            <Row style={{paddingTop: '10px'}}>
-              <Col span={9} offset={2}>
-                <Radio.Group value={this.state.size} onChange={this.handleSizeChange} style={{width:'100%',height:'125px'}}>
-                  <Radio.Button value="收入审查" style={{width:'100%',height:'125px',borderRadius:'5px',textAlign:'center'}}>
-                    <span style={{display:'block',width: '100%',fontSize:'14px',textAlign:'center'}}>收入审查</span>
-                    过去两年内您的个人年总收入超过20万美金，或与配偶联合年总收入超过30万美金，同时今年收入预期和前两年接近。
-                  </Radio.Button>
-                </Radio.Group>
-              </Col>
+
+        <FormItem
+          {...formItemLayout}
+          label=""
+        >
+          {getFieldDecorator('type', {
+            initialValue: "INCOME",
+          })(
+            <Radio.Group onChange={this.handleSizeChange} style={{width: '748px', marginLeft: '76px'}}>
+              <Radio.Button span={6} offset={5} value="INCOME" style={{
+                width: '340px',
+                height: '125px',
+                borderRadius: '5px',
+                textAlign: 'center',
+                float: 'left'
+              }}><span style={{display:'block',width: '100%',fontSize:'14px',textAlign:'center'}}>收入审查</span>
+                过去两年内您的个人年总收入超过20万美金，或与配偶联合年总收入超过30万美金，同时今年收入预期和前两年接近。</Radio.Button>
+
+              <div style={{width: '68px', float: 'left', textAlign: 'center',lineHeight:'125px'}}>OR</div>
+
+              <Radio.Button span={6} offset={5} value="NET_WORTH" style={{
+                width: '340px',
+                height: '125px',
+                borderRadius: '5px',
+                textAlign: 'center',
+                float: 'left'
+              }}><span style={{display:'block',width: '100%',fontSize:'14px',textAlign:'center'}}>净值审查</span>
+                您个人或与配偶联合资产超过1百万美金，包括退休金、401K、IRA、任何现金、投资基金、股票、投资房产(但不包括自己现居住的一处房产)及其他任何资产。</Radio.Button>
+
+            </Radio.Group>
+          )}
+        </FormItem>
 
 
-              <Col span={2}>
-                <div style={{width: '100%',height:'125px',lineHeight:'125px',textAlign:'center',fontSize:'18px'}}>or</div>
-              </Col>
 
-
-              <Col span={9}>
-                <Radio.Group value={this.state.size} onChange={this.handleSizeChange} style={{width:'100%',height:'125px'}}>
-                  <Radio.Button value="净值审查" style={{width:'100%',height:'125px',borderRadius:'5px',textAlign:'center'}}>
-                    <span style={{display:'block',width: '100%',fontSize:'14px',textAlign:'center'}}>净值审查</span>
-                    您个人或与配偶联合资产超过1百万美金，包括退休金、401K、IRA、任何现金、投资基金、股票、投资房产(但不包括自己现居住的一处房产)及其他任何资产。
-                  </Radio.Button>
-                </Radio.Group>
-              </Col>
-            </Row>
-          </FormItem>
-
-
-
-          <FormItem {...formItemLayout}>
             <Row style={{}}>
               <Col span={9} offset={2}>
-                <div style={{width:'100%',height:'125px',lineHeight:'20px',color:'#cccccc'}} className={this.state.size=='收入审查'?'show':'hide'}>
+                <div style={{width:'100%',height:'180px',lineHeight:'20px',color:'#cccccc'}} className={this.state.size=='INCOME'?'show':'hide'}>
                   注：请向美信金融递交过去两年的Form W-2, Form 1099, Schedule K-1 of Form 1065或已填完的Form 1040，来证明您的收入或与配偶的合计收入。您可以修改上述文件非必要部分以免泄露个人身份信息。
 
                 </div>
               </Col>
               <Col span={2}></Col>
               <Col span={9}>
-                <div style={{width:'100%',height:'125px',lineHeight:'20px',color:'#cccccc'}} className={this.state.size=='净值审查'?'show':'hide'}>
-                  <Row>
-                    <Col span={8}>
-                      <p style={{fontSize:'16px',color:'#223976',lineHeight:'30px',textAlign:'center'}}>负债金额</p>
-                    </Col>
+                <div style={{width:'100%',height:'180px',lineHeight:'20px',color:'#cccccc'}} className={this.state.size=='NET_WORTH'?'show':'hide'}>
 
-                    <Col  span={16}>
-                      <span style={{display:'inline-block',width:'200px',height:'30px',color:'#223976',fontSize:'16px',border:'1px solid #cccccc'}}>&nbsp;$ <input type="number" step="100" min="0" onChange={this.Change} style={{display:'inline',width:'180px',height:'27px',fontSize:'16px',color:'#ff6600',border:'none',outline:'none',textAlign:'center'}} />
-                      </span>
-                    </Col>
+
+                  <Row>
+                    <FormItem
+                      {...formItemLayout}
+                      label="负债金额 $"
+                      labelCol={{span: 8}}
+                      wrapperCol={{span: 14}}
+                    >
+                      {getFieldDecorator('debt_amount', {
+                        initialValue: 0,
+                        rules: [{
+                          required: false, message: '请输入您的负债金额!',
+                        }],
+                      })(
+                        <Input type="number" min="0" step="100" size="large" style={{width: 200,textAlign:'center'}} />
+                      )}
+                    </FormItem>
                   </Row>
 
                   <p>
@@ -142,13 +156,25 @@ class CompliancePublic extends React.Component {
                 </div>
               </Col>
             </Row>
-          </FormItem>
 
-          <FormItem {...formItemLayout}>
-            <Row style={{paddingTop: '30px'}}>
-              <Col span={20} offset={2}><Checkbox style={{color:'#223976'}} onChange={this.onChange}>我的审查资料包括配偶信息</Checkbox></Col>
-            </Row>
-          </FormItem>
+
+
+
+        <FormItem
+          {...formItemLayout}
+          wrapperCol={{span: 20,offset:2}}
+        >
+          {getFieldDecorator('with_spouse', {
+          })(
+            <Checkbox style={{color:'#223976'}} onChange={this.onChange}>我的审查资料包括配偶信息</Checkbox>
+          )}
+
+        </FormItem>
+
+
+
+
+
           <FormItem>
             <Row style={{}}>
               <Col span={20} offset={2}><p style={{width:'100%',height:'1px',backgroundColor:'#223976'}}></p></Col>
