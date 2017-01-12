010606b8299c0c36194df04311ab7622317db3f2
jingyu.zhang@meixinfinance.com <jingyu.zhang@meixinfinance.com>
2017-01-12 16:13:56 +0800

    Merge branch 'dev' of git.meixincn.com:web/acadia into dev
    # Conflicts:
    #	src/view/BasicInformation.js

--- src/view/AddressProof.js
+++ src/view/AddressProof.js
@@ -78,7 +78,7 @@ class AddressInformation extends React.Component {
 
 
 
-                    {!this.props.single?
+                    {this.props.single==false?
                         <Row style={{marginTop: '50px', paddingBottom: '40px'}}>
             <Col span={3} offset={6}>
               <Button style={{
