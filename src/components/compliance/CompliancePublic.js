6ead5feb21699b215d043d0d0be746af5afdaa9d
jingyu.zhang@meixinfinance.com <jingyu.zhang@meixinfinance.com>
2017-01-16 19:10:17 +0800

    Merge branch 'dev' of git.meixincn.com:web/acadia into dev
    # Conflicts:
    #	src/Redux/actions/index.js

--- src/components/compliance/CompliancePublic.js
+++ src/components/compliance/CompliancePublic.js
@@ -5,7 +5,8 @@
 require('components/compliance/CompliancePublic.css');
 import React from 'react';
 import {connect} from 'react-redux'
-import {Form, Row, Col,DatePicker,Checkbox,Button,Radio,Input} from 'antd';
+import {saveFields} from '../../Redux/actions/index'
+import {Form, Row, Col, DatePicker, Checkbox, Button, Radio, Input} from 'antd';
 import ComplianceSpouse from './complianceSpouse'
 
 const FormItem = Form.Item;
@@ -13,72 +14,77 @@ const FormItem = Form.Item;
 // function onChange(e) {
 //   console.log(`checked = ${e.target.checked}`);
 // }
-
 class CompliancePublic extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      a:false,
-      profile:{},
-      size: 'INCOME',
-      b:''
+    constructor(props) {
+        super(props)
     }
-  }
 
-  onChange = (e) => {
-    this.setState({ a: e.target.checked });
-    console.log(e.target.checked)
+    onChange = (e) => {
 
-  }
+        const {dispatch} = this.props
+        dispatch(saveFields('accreditation', {
+            ...this.props.getsProfile.base_profile.accreditation,
+            with_spouse: e.target.checked
+        }))
 
-  Change = (e) => {
-    this.setState({ b: e.target.value });
-    console.log("负债金额为 "+e.target.value)
-  }
+    }
 
-
-  handleSizeChange = (e) => {
-    this.setState({ size: e.target.value });
-    console.log(e.target.value)
-  }
-
-
-
-
-  handleSubmit(e) {
-    e.preventDefault();
-    this.props.form.validateFieldsAndScroll((err, values) => {
-      if (!err) {
-        console.log('Received values of form: ', values);
-      }
-    });
-  }
+    Change = (e) => {
+        const {dispatch} = this.props
+        dispatch(saveFields('accreditation', {
+            ...this.props.getsProfile.base_profile.accreditation,
+            debt_amount: e.target.value
+        }))
+        console.log("负债金额为 " + e.target.value)
+    }
 
 
-  render() {
-    const {getFieldDecorator} = this.props.getFieldDecorator;
-    // console.log(getFieldDecorator)
+    handleSizeChange = (e) => {
+        const {dispatch} = this.props
+        dispatch(saveFields('accreditation', {
+            ...this.props.getsProfile.base_profile.accreditation,
+            type: e.target.value
+        }))
+        console.log(e.target.value)
+    }
 
 
-    const formItemLayout = {
-      // labelCol: {span: 6},
-      // wrapperCol: {span: 14},
-    };
-    const tailFormItemLayout = {
-      wrapperCol: {
-        span: 14,
-        offset: 6,
-      },
-    };
-    return (
-      <div style={{width: 900, background: '#fff', overflow: 'hidden'}}>
+    handleSubmit(e) {
+        e.preventDefault();
+        this.props.form.validateFieldsAndScroll((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+            }
+        });
+    }
+
+
+    render() {
+        let baseData=this.props.getsProfile.base_profile.accreditation
+        const {getFieldDecorator} = this.props.getFieldDecorator;
+        // console.log(getFieldDecorator)
+
+
+        const formItemLayout = {
+            // labelCol: {span: 6},
+            // wrapperCol: {span: 14},
+        };
+        const tailFormItemLayout = {
+            wrapperCol: {
+                span: 14,
+                offset: 6,
+            },
+        };
+        return (
+            <div style={{width: 900, background: '#fff', overflow: 'hidden'}}>
 
           <FormItem>
             <Row style={{paddingTop: '30px'}}>
               <Col span={6} offset={2}><h2 style={{color: '#159bd6', fontFamily: '宋体'}}>投资人合规审查</h2></Col>
             </Row>
             <Row style={{}}>
-              <Col span={20} offset={2}><p style={{color: '#ff6600', fontFamily: '宋体', marginTop: '2px',lineHeight:'12px'}}>
+              <Col span={20} offset={2}><p
+                  style={{color: '#ff6600', fontFamily: '宋体', marginTop: '2px', lineHeight: '12px'}}>
                 为了确认您的自然投资人身份，您需进行收入或净值审查。</p></Col>
             </Row>
           </FormItem>
@@ -86,31 +92,31 @@ class CompliancePublic extends React.Component {
 
 
         <FormItem
-          {...formItemLayout}
-          label=""
+            {...formItemLayout}
+            label=""
         >
           {getFieldDecorator('type', {
-            initialValue: "INCOME",
+              initialValue: baseData.type||"INCOME",
           })(
-            <Radio.Group onChange={this.handleSizeChange} style={{width: '748px', marginLeft: '76px'}}>
+              <Radio.Group onChange={this.handleSizeChange.bind(this)} style={{width: '748px', marginLeft: '76px'}}>
               <Radio.Button span={6} offset={5} value="INCOME" style={{
-                width: '340px',
-                height: '125px',
-                borderRadius: '5px',
-                textAlign: 'center',
-                float: 'left'
-              }}><span style={{display:'block',width: '100%',fontSize:'14px',textAlign:'center'}}>收入审查</span>
+                  width: '340px',
+                  height: '125px',
+                  borderRadius: '5px',
+                  textAlign: 'center',
+                  float: 'left'
+              }}><span style={{display: 'block', width: '100%', fontSize: '14px', textAlign: 'center'}}>收入审查</span>
                 过去两年内您的个人年总收入超过20万美金，或与配偶联合年总收入超过30万美金，同时今年收入预期和前两年接近。</Radio.Button>
 
-              <div style={{width: '68px', float: 'left', textAlign: 'center',lineHeight:'125px'}}>OR</div>
+              <div style={{width: '68px', float: 'left', textAlign: 'center', lineHeight: '125px'}}>OR</div>
 
               <Radio.Button span={6} offset={5} value="NET_WORTH" style={{
-                width: '340px',
-                height: '125px',
-                borderRadius: '5px',
-                textAlign: 'center',
-                float: 'left'
-              }}><span style={{display:'block',width: '100%',fontSize:'14px',textAlign:'center'}}>净值审查</span>
+                  width: '340px',
+                  height: '125px',
+                  borderRadius: '5px',
+                  textAlign: 'center',
+                  float: 'left'
+              }}><span style={{display: 'block', width: '100%', fontSize: '14px', textAlign: 'center'}}>净值审查</span>
                 您个人或与配偶联合资产超过1百万美金，包括退休金、401K、IRA、任何现金、投资基金、股票、投资房产(但不包括自己现居住的一处房产)及其他任何资产。</Radio.Button>
 
             </Radio.Group>
@@ -121,30 +127,33 @@ class CompliancePublic extends React.Component {
 
             <Row style={{}}>
               <Col span={9} offset={2}>
-                <div style={{width:'100%',height:'180px',lineHeight:'20px',color:'#cccccc'}} className={this.state.size=='INCOME'?'show':'hide'}>
+                <div style={{width: '100%', height: '180px', lineHeight: '20px', color: '#cccccc'}}
+                     className={baseData.type == 'INCOME' ? 'show' : 'hide'}>
                   注：请向美信金融递交过去两年的Form W-2, Form 1099, Schedule K-1 of Form 1065或已填完的Form 1040，来证明您的收入或与配偶的合计收入。您可以修改上述文件非必要部分以免泄露个人身份信息。
 
                 </div>
               </Col>
               <Col span={2}></Col>
               <Col span={9}>
-                <div style={{width:'100%',height:'180px',lineHeight:'20px',color:'#cccccc'}} className={this.state.size=='NET_WORTH'?'show':'hide'}>
+                <div style={{width: '100%', height: '180px', lineHeight: '20px', color: '#cccccc'}}
+                     className={baseData.type == 'NET_WORTH' ? 'show' : 'hide'}>
 
 
                   <Row>
                     <FormItem
-                      {...formItemLayout}
-                      label="负债金额 $"
-                      labelCol={{span: 8}}
-                      wrapperCol={{span: 14}}
+                        {...formItemLayout}
+                        label="负债金额 $"
+                        labelCol={{span: 8}}
+                        wrapperCol={{span: 14}}
                     >
                       {getFieldDecorator('debt_amount', {
-                        initialValue: 0,
-                        rules: [{
-                          required: false, message: '请输入您的负债金额!',
-                        }],
+                          initialValue: baseData.debt_amount||0,
+                          rules: [{
+                              required: false, message: '请输入您的负债金额!',
+                          }],
                       })(
-                        <Input type="number" min="0" step="100" size="large" style={{width: 200,textAlign:'center'}} />
+                          <Input type="number" min="0" step="100" size="large"
+                                 style={{width: 200, textAlign: 'center'}} />
                       )}
                     </FormItem>
                   </Row>
@@ -162,15 +171,15 @@ class CompliancePublic extends React.Component {
 
 
         <FormItem
-          {...formItemLayout}
-          wrapperCol={{span: 20,offset:2}}
+            {...formItemLayout}
+            wrapperCol={{span: 20, offset: 2}}
         >
           {getFieldDecorator('with_spouse', {
-            valuePropName: '',
-            initialValue: false,
-            defaultChecked:false
+              valuePropName: '',
+              initialValue: baseData.with_spouse||false,
+              defaultChecked: false
           })(
-            <Checkbox style={{color:'#223976'}} onChange={this.onChange}>我的审查资料包括配偶信息</Checkbox>
+              <Checkbox style={{color: '#223976'}} onChange={this.onChange.bind(this)}>我的审查资料包括配偶信息</Checkbox>
           )}
 
         </FormItem>
@@ -181,57 +190,35 @@ class CompliancePublic extends React.Component {
 
           <FormItem>
             <Row style={{}}>
-              <Col span={20} offset={2}><p style={{width:'100%',height:'1px',backgroundColor:'#223976'}}></p></Col>
+              <Col span={20} offset={2}><p style={{width: '100%', height: '1px', backgroundColor: '#223976'}}></p></Col>
             </Row>
           </FormItem>
 
 
-
-          {this.state.a?
-            <ComplianceSpouse {...this.props}  getFieldDecorator={this.props.getFieldDecorator}></ComplianceSpouse>
-          :
-          ''
-          }
-
-
+                {baseData.with_spouse ?
+                    <ComplianceSpouse {...this.props}
+                                      getFieldDecorator={this.props.getFieldDecorator}></ComplianceSpouse>
+                    :
+                    ''
+                }
 
 
-
-
-
-
-          <FormItem>
+                <FormItem>
             <Row style={{}}>
-              <Col span={20} offset={2}><p style={{color: '#ff6600', fontFamily: '宋体', marginTop: '2px',lineHeight:'12px'}}>
+              <Col span={20} offset={2}><p
+                  style={{color: '#ff6600', fontFamily: '宋体', marginTop: '2px', lineHeight: '12px'}}>
                 注：如您需要向美信金融递交上述合格投资人证明材料，请您用邮件发送至invest@meixinfinance.com。</p></Col>
             </Row>
           </FormItem>
-
-          {/*// <FormItem {...tailFormItemLayout}>*/}
-          {/*//   <Row style={{marginTop: '50px', paddingBottom: '40px'}}>*/}
-          {/*//     <Col span={4} offset={7}>*/}
-          {/*//       <Button style={{*/}
-          {/*//                           width: '120px',*/}
-          {/*//                           height: '50px',*/}
-          {/*//                           borderRadius: '30px',*/}
-          {/*//                           background: '#223976',*/}
-          {/*//                           color: '#fff',*/}
-          {/*//                           fontSize: '18px'*/}
-          {/*//                       }} type="primary" htmlType="submit" onClick= {this.handleSubmit.bind(this)} size="large">完成</Button>*/}
-              {/*/!*</Col>*!/*/}
-            {/*/!*</Row>*!/*/}
-          {/*/!*</FormItem>*!/*/}
-
-
       </div>
-    );
-  }
+        );
+    }
 }
 
 const mapStateToProps = (state) => {
-  return {
-    getsProfile: state.getsProfile
-  }
+    return {
+        getsProfile: state.getsProfile
+    }
 }
 CompliancePublic.defaultProps = {};
 export default connect(mapStateToProps)(CompliancePublic)
--- src/components/compliance/ComplianceSpouse.js
+++ src/components/compliance/ComplianceSpouse.js
@@ -1,152 +1,142 @@
-/**
- * Created by robot on 2016/12/20.
- */
 import React from 'react';
 import {connect} from 'react-redux'
-import {Form, Input, Select, Row, Col, Button,DatePicker} from 'antd';
+import {Form, Input, Select, Row, Col, Button, DatePicker} from 'antd';
 
 
 const FormItem = Form.Item;
 const Option = Select.Option;
 
 function handleChange(value) {
-  console.log(`selected ${value}`);
+    console.log(`selected ${value}`);
 }
-
+let countyData
 class ComplianceSpouse extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      profile:{}
+    constructor(props) {
+        super(props)
+        countyData = props.getsProfile.Country.map(function (data,index) {
+            return <Option value={data.cellcode+' %%'+data.name} key={data.cellcode+index+data.name}>{data.name}</Option>
+        })
     }
-  }
-  handleSubmit(e) {
-    e.preventDefault();
-    this.props.form.validateFieldsAndScroll((err, values) => {
-      if (!err) {
-        console.log('Received values of form: ', values);
-      }
-    });
-  }
 
-  render() {
+    handleSubmit(e) {
+        e.preventDefault();
+        this.props.form.validateFieldsAndScroll((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+            }
+        });
+    }
 
-    const { size } = this.props;
-    const state = this.state;
-    const {getFieldDecorator} = this.props.getFieldDecorator;
-    console.log(getFieldDecorator)
-    const formItemLayout = {
-      labelCol: {span: 6},
-      wrapperCol: {span: 14},
-    };
-    const tailFormItemLayout = {
-      wrapperCol: {
-        span: 14,
-        offset: 6,
-      },
-    };
+    countryChange(e) {
+        let country = e.split('%%')[0]
+        console.log(country)
+
+    }
+
+    render() {
+        let baseData = this.props.getsProfile.base_profile.accreditation
+        const {getFieldDecorator} = this.props.getFieldDecorator;
+        const formItemLayout = {
+            labelCol: {span: 6},
+            wrapperCol: {span: 14},
+        };
+        const tailFormItemLayout = {
+            wrapperCol: {
+                span: 14,
+                offset: 6,
+            },
+        };
 
 
-
-    const prefixSelector = getFieldDecorator('prefix', {
-      initialValue: '+86',
-    })(
-      <Select className="icp-selector" onChange={this.phone}>
-        <Option value="86">+86</Option>
-        <Option value="1">+1</Option>
+/*        const prefixSelector = getFieldDecorator('prefix', {
+            initialValue: '中国',
+        })(
+            <Select className="icp-selector"  onChange={this.countryChange.bind(this)}>
+                {countyData}
       </Select>
-    );
+        );*/
 
 
-    return (
-      <div style={{width: 900, background: '#fff', overflow: 'hidden'}}>
+        return (
+            <div style={{width: 900, background: '#fff', overflow: 'hidden'}}>
 
           <FormItem>
             <Row style={{paddingTop: '0px'}}>
-              <Col span={20} offset={2}><p style={{color:'#223976',textAlign:'center'}}>请完成一下配偶相关信息</p></Col>
+              <Col span={20} offset={2}><p style={{color: '#223976', textAlign: 'center'}}>请完成一下配偶相关信息</p></Col>
             </Row>
           </FormItem>
           <Row>
             <Col span={12}>
               <FormItem
-                {...formItemLayout}
-                label='姓氏'
-                labelCol={{span: 4,offset: 4}}
-                wrapperCol={{span: 14}}
+                  {...formItemLayout}
+                  label='姓氏'
+                  labelCol={{span: 4, offset: 4}}
+                  wrapperCol={{span: 14}}
               >
                 {getFieldDecorator('spouse_last_name', {
-                  rules: [{
-                    required: true, message: '请输入您配偶姓的汉语拼音!',
-                  },{pattern: /^[a-z \,\.\-\']+$/i, message: '请输入汉语拼音!',}],
+                    initialValue: baseData.spouse_last_name||'',
+                    rules: [{
+                        required: baseData.with_spouse?true:false,
+                        message: '请输入您配偶姓的汉语拼音!',
+                    }, {pattern: /^[a-z \,\.\-\']+$/i, message: '请输入汉语拼音!',}],
                 })(
-                  <Input type='text' size='large' style={{width: 240}}/>
+                    <Input type='text' size='large' style={{width: 240}} />
                 )}
               </FormItem>
             </Col>
             <Col span={12}>
               <FormItem
-                {...formItemLayout}
-                label='名'
-                labelCol={{span: 4,offset: 2}}
-                wrapperCol={{span: 14}}
+                  {...formItemLayout}
+                  label='名'
+                  labelCol={{span: 4, offset: 2}}
+                  wrapperCol={{span: 14}}
               >
                 {getFieldDecorator('spouse_first_name', {
-                  //initialValue:'yang',
-                  rules: [{
-                    required: true, message: '请输入您配偶名的汉语拼音!',
-                  },{pattern: /^[a-z \,\.\-\']+$/i, message: '请输入汉语拼音!',}],
+                    initialValue: baseData.spouse_first_name||'',
+                    rules: [{
+                        required: baseData.with_spouse?true:false,
+                        message: '请输入您配偶名的汉语拼音!',
+                    }, {pattern: /^[a-z \,\.\-\']+$/i,
+                        message: '请输入汉语拼音!',}],
                 })(
-                  <Input type='text'  size='large' style={{width: 240}}/>
+                    <Input type='text' size='large' style={{width: 240}} />
                 )}
               </FormItem>
             </Col>
           </Row>
           <Row>
             <Col span={12}>
-
-
-
               <FormItem
-                {...formItemLayout}
-                label="电话"
-                labelCol={{span: 4,offset: 4}}
-                wrapperCol={{span: 14}}
+                  {...formItemLayout}
+                  label="电话"
+                  labelCol={{span: 4, offset: 4}}
+                  wrapperCol={{span: 14}}
               >
-                {getFieldDecorator('phone', {
-                  rules: [{ required: true, message: '请输入您配偶的电话号码！' }],
+                {getFieldDecorator('spouse_phone', {
+                    initialValue: baseData.spouse_phone||'',
+                    rules: [{required: baseData.with_spouse?true:false,
+                        message: '请输入您配偶的电话号码！'}],
                 })(
-                  <Input addonBefore={prefixSelector} style={{width: 170}}/>
+                    <Input style={{width: 240}} />
                 )}
               </FormItem>
-
-
-
-
-
-
-
-
-
-
-
-
-
             </Col>
             <Col span={12}>
               <FormItem
-                {...formItemLayout}
-                label='邮箱'
-                labelCol={{span: 4,offset: 2}}
-                wrapperCol={{span: 14}}
+                  {...formItemLayout}
+                  label='邮箱'
+                  labelCol={{span: 4, offset: 2}}
+                  wrapperCol={{span: 14}}
               >
                 {getFieldDecorator('spouse_email', {
-                  //initialValue:'yang',
-                  rules: [{
-                    type:'email',
-                    required: true, message: '请输入您配偶的邮箱！',
-                  }],
+                    initialValue: baseData.spouse_email||'',
+                    rules: [{
+                        type: 'email',
+                        required: baseData.with_spouse?true:false,
+                        message: '请输入您配偶的邮箱！',
+                    }],
                 })(
-                  <Input type='text' size='large' style={{width: 240}}/>
+                    <Input type='text' size='large' style={{width: 240}} />
                 )}
               </FormItem>
             </Col>
@@ -154,42 +144,42 @@ class ComplianceSpouse extends React.Component {
 
 
       </div>
-    );
-  }
+        );
+    }
 }
 
 
 const mapStateToProps = (state) => {
-  return {
-    getsProfile: state.getsProfile
-  }
+    return {
+        getsProfile: state.getsProfile
+    }
 }
 ComplianceSpouse.defaultProps = {};
 export default connect(mapStateToProps)(ComplianceSpouse)
 
 
 /***************
-<FormItem
-{...formItemLayout}
-label='电话'
-labelCol={{span: 4,offset: 4}}
-wrapperCol={{span: 13}}
->
-<span>
-<Select
-value={state.currency}
-size={size}
-style={{ width: '32%',marginRight:'3%'}}
->
-<Option value='rmb'>RMB</Option>
-  <Option value='dollar'>Dollar</Option>
-  </Select>
-  <Input
-type='text'
-size={size}
-value={state.number}
-style={{ width: '64%',height:'32px'}}
-/>
-</span>
-</FormItem>
-  *********************/
+ <FormItem
+ {...formItemLayout}
+ label='电话'
+ labelCol={{span: 4,offset: 4}}
+ wrapperCol={{span: 13}}
+ >
+ <span>
+ <Select
+ value={state.currency}
+ size={size}
+ style={{ width: '32%',marginRight:'3%'}}
+ >
+ <Option value='rmb'>RMB</Option>
+ <Option value='dollar'>Dollar</Option>
+ </Select>
+ <Input
+ type='text'
+ size={size}
+ value={state.number}
+ style={{ width: '64%',height:'32px'}}
+ />
+ </span>
+ </FormItem>
+ *********************/
